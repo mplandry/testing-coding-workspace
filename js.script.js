4f8028786d7f95994b257c2d00a1d52b8ee9b360
Michael Landry <59461136+mplandry@users.noreply.github.com>
2023-04-21 17:04:44 -0400

    Workspace

---
+++ js.script.js
@@ -0,0 +1,21 @@
+const companies = [
+  { name: "Company Two", category: "Technology", start: 1981, end: 1997 },
+  { name: "Company One", category: "Finance", start: 2000, end: 2003 },
+  { name: "Company Three", category: "Auto", start: 1981, end: 2002 },
+  { name: "Company Four", category: "Finance", start: 1997, end: 2003 },
+  { name: "Company Five", category: "Finance", start: 1989, end: 2023 },
+  { name: "Company Six", category: "Technology", start: 1981, end: 2003 },
+  { name: "Company Seven", category: "Auto", start: 1981, end: 2003 },
+  { name: "Company Eight", category: "Finance", start: 1969, end: 2003 },
+  { name: "Company Nine", category: "Technology", start: 1981, end: 2003 },
+  { name: "Company Ten", category: "Auto", start: 1981, end: 1997 },
+];
+
+const ages = [34, 89, 02, 35, 90, 64, 35, 32, 64, 84, 21, 12, 05, 04, 08];
+
+//forEach
+//filter
+//map
+//sort
+//reduce
+//some
